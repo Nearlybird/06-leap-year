e1a96034c106f849f4da31ad37483105df4b175f
Nearlybird <49175582+Nearlybird@users.noreply.github.com>
2019-04-15 18:25:16 -0600

    Update leap_year.py
    Oh my gosh. I completely forgot about this. I did the graded exercise after I read the chapter but before it went up on Github. I stored this as a repository on here but forgot to post it. I submitted this exercise  before we reviewed it in class.

--- leap_year.py
+++ leap_year.py
@@ -4,5 +4,13 @@
 
 # Don't forget to reach out for help after your own due diligence
 
-def is_leap_year(year):
-    return ''
+def is_leap(year):
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+        else: 
+            return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
